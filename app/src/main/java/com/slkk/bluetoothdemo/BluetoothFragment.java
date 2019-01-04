37627da5050d54f93bed7b7f3383ab8cca1e7130
slkk <644187060@qq.com>
2019-01-04 18:04:28 +0800

    init BluetoothFragment

--- app/src/main/java/com/slkk/bluetoothdemo/BluetoothFragment.java
+++ app/src/main/java/com/slkk/bluetoothdemo/BluetoothFragment.java
@@ -1,19 +1,31 @@
 package com.slkk.bluetoothdemo;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class BluetoothFragment extends Fragment {
     private static final String TAG = "BluetoothFragment";
+    private BluetoothAdapter mDefaultAdapter;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        mDefaultAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mDefaultAdapter == null) {
+            FragmentActivity activity = getActivity();
+            Toast.makeText(activity, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            activity.finish();
+        }
     }
 
 
