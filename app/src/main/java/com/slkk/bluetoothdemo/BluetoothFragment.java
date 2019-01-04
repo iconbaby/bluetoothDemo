49b5382c13ca4bfd13e19d286c2214f7c4669eb1
slkk <644187060@qq.com>
2019-01-04 18:55:25 +0800

    init bluetoothService

--- app/src/main/java/com/slkk/bluetoothdemo/BluetoothFragment.java
+++ app/src/main/java/com/slkk/bluetoothdemo/BluetoothFragment.java
@@ -1,20 +1,52 @@
 package com.slkk.bluetoothdemo;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BluetoothFragment extends Fragment {
     private static final String TAG = "BluetoothFragment";
     private BluetoothAdapter mDefaultAdapter;
+    private static final int REQUST_ENABLE_BT = 1;
+    private ArrayAdapter mConversationAdapter;
+    private ListView mListviewChat;
+    private Button mBtnSend;
+    private EditText mEtInput;
 
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
+    private TextView.OnEditorActionListener mWriteListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
+                String message = v.getText().toString();
+                sendMessage(message);
+            }
+            return true;
+        }
+    };
+    private BluetoothService mBluetoothService;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,11 +60,50 @@ public class BluetoothFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!mDefaultAdapter.isEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, REQUST_ENABLE_BT);
+        } else {
+            setupChat();
+        }
+    }
+
+
+    private void setupChat() {
+        mConversationAdapter = new ArrayAdapter<String>(getActivity(), R.layout.message);
+        mListviewChat.setAdapter(mConversationAdapter);
+
+        mEtInput.setOnEditorActionListener(mWriteListener);
+
+        mBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = mEtInput.getText().toString().trim();
+                sendMessage(inputText);
+            }
+        });
+
+        mBluetoothService = new BluetoothService(getActivity(), mHandler);
+    }
+
+    private void sendMessage(String inputText) {
+        
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bluetooth, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mListviewChat = view.findViewById(R.id.fragment_bluetooth_lv_chat);
+        mBtnSend = view.findViewById(R.id.fragment_bluetooth_bt_send);
+        mEtInput = view.findViewById(R.id.fragment_bluetooth_et_input);
     }
 }
---
+++ app/src/main/java/com/slkk/bluetoothdemo/BluetoothService.java
@@ -0,0 +1,10 @@
+package com.slkk.bluetoothdemo;
+
+import android.content.Context;
+import android.os.Handler;
+
+public class BluetoothService {
+    public BluetoothService(Context context, Handler handler) {
+
+    }
+}
